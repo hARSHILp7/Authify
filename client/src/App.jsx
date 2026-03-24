dae81877e71e533208f1404adae6191228c97403
Harshil Prajapati <prajapatiharshil3107@gmail.com>
2026-03-24 09:55:26 -0400

    Delete unnecessary files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-black">
-      <div className="text-4xl font-bold text-white">Hello World</div>
+      <div className="text-4xl font-bold text-white">Hello world</div>
     </div>
   )
 }
