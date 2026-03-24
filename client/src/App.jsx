e3c5bafedb7d85649ae4d6e549291520a1471cb3
Harshil Prajapati <prajapatiharshil3107@gmail.com>
2026-03-23 22:16:37 -0400

    React setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black">
+      <div className="text-4xl font-bold text-white">Hello World</div>
+    </div>
+  )
+}
+
+export default App
